ced160c76e11d6faf4214cd3d865ca88257099cf
“erhaneth” <gumusucb21@gmail.com>
2022-10-13 20:51:30 -0700

    Solve the nemo

---
+++ solveDodgerBlue.js
@@ -0,0 +1,10 @@
+const nemo = ['nemo'];
+
+function findNemo(array) {
+    for (let i =0; i < array.lentgh; i++ ){
+        if(array[i]==='nemo'){
+            console.log("Found NEMO!");
+        }
+    }
+}
+findNemo(nemo)
