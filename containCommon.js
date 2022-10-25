a7bece0fc181b9694ce840cb05a86e1b1ca9c364
“erhaneth” <gumusucb21@gmail.com>
2022-10-24 22:02:01 -0700

    not efficient way

--- containCommon.js
+++ containCommon.js
@@ -8,3 +8,17 @@
 
 // 2 parameters - arrays - no size limit
 // return true or false
+const arr1 = ['a', 'b', 'c', 'x'];
+const arr2 = ['z', 'y', 'i'];
+function containCommonItem(arr1,arr2){
+    for (let i = 0; i < arr1.length; i++){
+        for (let j = 0; j < arr2.length; j++){
+            if (arr1[i]===arr2[j]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(containCommonItem(arr1, arr2))
